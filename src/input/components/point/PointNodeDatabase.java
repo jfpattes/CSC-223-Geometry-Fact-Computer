bc58d0548de8a4eda7fb33b779553910756ec833
Moultrie Dangerfield <moultriedanger@gmail.com>
2023-09-05 15:05:58 -0400

    Add getName methods to pointNodeDatabase

--- src/input/components/point/PointNodeDatabase.java
+++ src/input/components/point/PointNodeDatabase.java
@@ -2,7 +2,6 @@ package input.components.point;
 
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
-import java.lang.Throwable;
 
 public class PointNodeDatabase {
 	
@@ -15,17 +14,41 @@ public class PointNodeDatabase {
 	public PointNodeDatabase(ArrayList<PointNode> _list) {
 		_points = new LinkedHashSet<PointNode>(_list);
 	}
+	
 	public void put(PointNode node) {
 		_points.add(node);
 	}
+	
 	public boolean contains(PointNode node) {
 		return _points.contains(node);
 	}
+	
 	public boolean contains(int x, int y) {
 		return this.contains(new PointNode(x, y));
 	}
 	
+	public String getName(PointNode node) {
+		
+		for (PointNode p: _points) {
+			if(p.getName().equals(node)) {
+				return p.getName();
+			}
+		}
+		return null;
+	}
+	
+	public String getName(double x, double y) {
+		
+		for(PointNode p: _points) {
+			if(p.getX() == x && p.getY() == y) {
+				return p.getName();
+			}
+		}
+		return null;
+	}
+	
 	public PointNode getPoint(PointNode node) {
+		
 		for (PointNode p: _points) {
 			if(p.getName().equals(node)) {
 				return p;
@@ -35,6 +58,7 @@ public class PointNodeDatabase {
 	}
 	
 	public PointNode getPoint(double x, double y) {
+		
 		for(PointNode p: _points) {
 			if(p.getX() == x && p.getY() == y) {
 				return p;
