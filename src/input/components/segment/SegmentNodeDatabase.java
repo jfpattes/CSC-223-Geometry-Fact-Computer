a5e1da7d7a3978beab5f9ca18ad321050c9fb6a0
jfpattes <jfpattes@gmail.com>
2023-09-07 15:00:52 -0400

    SegmentNodeDatabase
    instance variable and constructors

--- src/input/components/segment/SegmentNodeDatabase.java
+++ src/input/components/segment/SegmentNodeDatabase.java
@@ -1,5 +1,20 @@
 package input.components.segment;
 
-public class SegmentNodeDatabase {
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
 
+import input.components.point.PointNode;
+
+public class SegmentNodeDatabase {
+		protected HashMap<PointNode, LinkedHashSet<PointNode>> _adjLists;
+		
+		public SegmentNodeDatabase() {
+			_adjLists= new HashMap<PointNode, LinkedHashSet<PointNode>>();
+		}
+		public SegmentNodeDatabase(HashMap<PointNode, LinkedHashSet<PointNode>> map) {
+			_adjLists= new HashMap<PointNode, LinkedHashSet<PointNode>>(map);
+		}
+		
+	
 }
